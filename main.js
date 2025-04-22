45dc7c381dfb6232b339164fbe5502d493c912c6
davi campos jaremczuk <147657664+Tibursinho@users.noreply.github.com>
2025-04-22 13:30:58 -0300

    vf

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
     } else {
-        return [1, 0, 0, 0];
+        return [12, 8, 10, 0];
     }
 }
 
